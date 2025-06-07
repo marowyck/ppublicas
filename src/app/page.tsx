f72d8e2a3d7ff527ef72fb871a78e502bb5b7c28
marowyck <marowyck@gmail.com>
2025-06-07 13:41:14 -0300

    fix: fixing imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,8 @@ import Examples from "@/(app)/examples/page";
 import Footer from "@/(app)/footer/page";
 import IA from "@/(app)/ia/page";
 import Importance from "@/(app)/importance/page";
-import Reference from "@/app/references/page";
 import Resume from "@/(app)/resume/page";
 import TopBar from "@/(app)/topbar/page";
-import Link from "next/link";
 
 export default function Home() {
   return (
