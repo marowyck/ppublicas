6a8d56975229214bd2affefb641596c01e74cad1
marowyck <marowyck@gmail.com>
2025-06-10 16:02:30 -0300

    feat: enhance References page with additional links and improved styling

--- src/app/references/page.tsx
+++ src/app/references/page.tsx
@@ -1,7 +1,49 @@
 export default function References() {
   return (
-    <div>
-      <h1>Referências</h1>
+    <div className="flex flex-col p-6 justify-center">
+      <h1 className="font-bold text-[22px] text-[#9b4890] text-center pb-12">Referências</h1>
+
+      <h1 className="font-bold text-[22px] text-[#000] text-center">
+        <a href="https://www.planalto.gov.br/ccivil_03/_ato2015-2018/2018/lei/l13709.htm" target="_blank" rel="noopener noreferrer">
+          Lei Geral de Proteção de Dados Pessoais (LGPD)
+        </a>
+      </h1>
+
+      <h1 className="font-bold text-[22px] text-[#000] text-center">
+        <a href="https://g1.globo.com/politica/noticia/2025/06/09/cpi-das-bets-vai-pedir-indiciamento-de-16-pessoas.ghtml" target="_blank" rel="noopener noreferrer">
+          CPI das Bets vai pedir indiciamento de 16 pessoas.
+        </a>
+      </h1>
+
+      <h1 className="font-bold text-[22px] text-[#000] text-center">
+        <a href="https://www.stj.jus.br/sites/portalp/Comunicacao/Politica-de-Uso-Rede-Sociais" target="_blank" rel="noopener noreferrer">
+          Política de Uso das Redes Sociais
+        </a>
+      </h1>
+
+      <h1 className="font-bold text-[22px] text-[#000] text-center">
+        <a href="https://www12.senado.leg.br/noticias/materias/2025/05/28/senado-aprova-restricao-a-publicidade-de-bets" target="_blank" rel="noopener noreferrer">
+          PL das Bets e regulação da publicidade em redes sociais
+        </a>
+      </h1>
+
+      <h1 className="font-bold text-[22px] text-[#000] text-center">
+        <a href="https://www12.senado.leg.br/noticias/materias/2025/04/23/cct-vai-avaliar-politicas-publicas-de-inteligencia-artificial" target="_blank" rel="noopener noreferrer">
+          CCT vai avaliar políticas públicas de inteligência artificial
+        </a>
+      </h1>
+
+      <h1 className="font-bold text-[22px] text-[#000] text-center">
+        <a href="https://www.uff.br/18-09-2024/estudo-analisa-desinformacao-a-partir-do-processo-de-criacaodo-pl-das-fake-news/" target="_blank" rel="noopener noreferrer">
+          Estudo analisa desinformação a partir do processo de criação do PL das fake news.
+        </a>
+      </h1>
+
+      <h1 className="font-bold text-[22px] text-[#000] text-center">
+        <a href="https://www.conjur.com.br/2023-mai-15/direito-eleitoral-pl-2630-caminhos-descaminhos-desinformacao/" target="_blank" rel="noopener noreferrer">
+          PL 2630: caminhos e descaminhos da desinformação.
+        </a>
+      </h1>
     </div>
   );
 }
